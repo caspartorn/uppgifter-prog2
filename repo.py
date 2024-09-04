f65d7dd59e792c7edfdb29f1eee72eedd4dcf1d7
caspartorn <150004511+caspartorn@users.noreply.github.com>
2024-09-04 10:02:09 +0200

    Add files via upload

---
+++ repo.py
@@ -0,0 +1,44 @@
+# def is_consonant(char):
+# return char.lower() in "bcdfghjklmnpqrstvwxyz"
+
+
+# def translate_to_rovarspraket(text):
+#     rovarspraket = ""
+#     for char in text:
+#         if is_consonant(char):
+#             rovarspraket += char + "o" + char.lower()
+#         else:
+#             rovarspraket += char
+#     return rovarspraket
+
+
+# input_text = input("Skriv in en mening på svenska: ")
+# translated_text = translate_to_rovarspraket(input_text)
+# print("Rövarspråket: ", translated_text)
+
+
+# text = input("Skriv in en text: ")
+
+# baklanges_text = text[::-1]
+
+# print("Texten baklänges: ", baklanges_text)
+
+# lander_huvdstäder = {
+#     "sverige": "stockholm",
+#     "norge": "oslo",
+#     "Finland": "hälsingfors",
+#     "island": "reykjavik",
+# }
+
+# lander_huvdstäder.update({"Danmark": "köpenhamn"})
+# lander_huvdstäder.pop("Finland")
+
+# set1 = {"Banana", "Pear", "Apple"}
+# set2 = {"Kiwi", "Pineapple", "Pear"}
+
+# result_set = set1.union(set2)
+
+
+# print("The resulting set:", result_set)
+
+# print("Number of elements in the resulting set:", len(result_set))
